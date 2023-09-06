0088ad1d30f64a92e5f7a1c57302ce03393fec62
Muhammad Abu Bakkar Bin Akmal <121852596+AbuBakkarBinAkmal@users.noreply.github.com>
2023-09-06 16:13:31 +0500

    Comments for handlers.go

--- drawTF/drawtf/handlers.go
+++ drawTF/drawtf/handlers.go
@@ -9,11 +9,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Message struct represents a JSON message with 'text' and 'path' fields.
 type Message struct {
 	Text string `json:"text"`
 	Path string `json:"path"`
 }
 
+// GenerateEmbeddings is an HTTP handler that generates embeddings based on the input message.
 func GenerateEmbeddings(c *gin.Context) {
 	var m Message
 	if err := c.ShouldBindJSON(&m); err != nil {
@@ -21,6 +23,7 @@ func GenerateEmbeddings(c *gin.Context) {
 		return
 	}
 
+	// Call a function to fetch configuration from VecStore.
 	response, err := fetchConfigFromVecStore(m.Text, m.Path, false)
 	if err != nil {
 		fmt.Println("error is", err)
@@ -30,10 +33,13 @@ func GenerateEmbeddings(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"response": response})
 }
 
+// GenerateTfconfig is an HTTP handler that generates a TensorFlow configuration based on query parameters and JSON request.
 func GenerateTfconfig(c *gin.Context) {
 
+	// Retrieve 'path' query parameter.
 	docsPath := c.Query("path")
-	// Retrieval Augmented Generation flag
+
+	// Retrieve 'rag' query parameter and parse it as a boolean.
 	isRag, err := strconv.ParseBool(c.Query("rag"))
 	fmt.Println("rag is   ", isRag)
 	if err != nil {
@@ -42,11 +48,14 @@ func GenerateTfconfig(c *gin.Context) {
 		return
 	}
 
+	// Parse the incoming JSON request.
 	var req json.RawMessage
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	// Call an integrated handler function with the parsed data.
 	refinementResponse, err := integratedHandler(req, docsPath, isRag)
 
 	if err != nil {
@@ -57,6 +66,7 @@ func GenerateTfconfig(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"response": refinementResponse})
 }
 
+// StartGradio is an HTTP handler that launches Gradio.
 func StartGradio(c *gin.Context) {
 	err := launchGradio()
 	if err != nil {

50dc2c0d9fe097454b19e80a01754e9be0813eb8
Muhammad Abu Bakkar Bin Akmal <121852596+AbuBakkarBinAkmal@users.noreply.github.com>
2023-09-06 16:16:09 +0500

    Added comments for utils.go

--- drawTF/drawtf/utils.go
+++ drawTF/drawtf/utils.go
@@ -8,6 +8,7 @@ import (
 	"time"
 )
 
+// readOutput reads the content of a file specified by the path and returns it as a string.
 func readOutput(path string) (string, error) {
 	// Open the file for reading
 	filePath := path
@@ -27,16 +28,18 @@ func readOutput(path string) (string, error) {
 	return string(content), nil
 }
 
+// saveToFile writes the given content to a file specified by the path.
 func saveToFile(content []byte, path string) error {
 	filepath := path
 	err := os.WriteFile(filepath, content, 0644)
 	if err != nil {
-		fmt.Println("Error writitng file:", err)
+		fmt.Println("Error writing file:", err)
 		return err
 	}
 	return nil
 }
 
+// timeDelta logs the time taken by a script and appends it to a "time.log" file.
 func timeDelta(start time.Time, name string) {
 	log.Println("\n*********************")
 	log.Printf("%s script called ", name)
@@ -44,22 +47,22 @@ func timeDelta(start time.Time, name string) {
 	time := fmt.Sprintf("%s takes: %s \n", name, time.Since(start)) + "\n\n"
 	file, err := os.OpenFile("time.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal("Cant write time benchmarks to the file. ", err)
+		log.Fatal("Can't write time benchmarks to the file. ", err)
 	}
 	defer file.Close()
 	file.Write([]byte(time))
 	log.Println("*********************\n")
-
 }
 
+// launchGradio launches the Gradio application using the specified Python interpreter and Gradio launcher.
 func launchGradio() error {
 	pythonCmd := exec.Command(PythonInterpreter, GradioLauncher)
 	pythonCmd.Stdout = os.Stdout
 	pythonCmd.Stderr = os.Stderr
 	err := pythonCmd.Start()
-	fmt.Println("errrror isssssssssssssss, ", err)
+	fmt.Println("Error is:", err)
 	if err != nil {
-		fmt.Println("Error running Gradio Launcher.:", err)
+		fmt.Println("Error running Gradio Launcher:", err)
 		return err
 	}
 	return nil

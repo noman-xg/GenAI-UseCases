04c5f6e061db8dae0aa8652604e7c8948a0ec1fe
Muhammad Abu Bakkar Bin Akmal <121852596+AbuBakkarBinAkmal@users.noreply.github.com>
2023-09-06 15:49:50 +0500

    Added comments for main.go

--- drawTF/main.go
+++ drawTF/main.go
@@ -1,15 +1,26 @@
+// Package main is the entry point for the Go program.
 package main
 
+// Import necessary packages for the application.
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/noman-xg/GenAI-UseCases/drawtf"
+	"github.com/gin-gonic/gin"                // Import the Gin web framework for building web APIs.
+	"github.com/noman-xg/GenAI-UseCases/drawtf" // Import a custom package, possibly containing application-specific logic.
 )
 
+// The main function is the entry point for the application.
 func main() {
+	// Create a new Gin router with default middleware.
 	r := gin.Default()
 
+	// Define HTTP routes and associate them with specific handler functions.
+	// These routes are used to handle incoming HTTP POST requests.
+	// - "/message" route is associated with the "GenerateEmbeddings" function from the "drawtf" package.
+	// - "/tfconfig" route is associated with the "GenerateTfconfig" function from the "drawtf" package.
+	// - "/start-gradio" route is associated with the "StartGradio" function from the "drawtf" package.
 	r.POST("/message", drawtf.GenerateEmbeddings)
 	r.POST("/tfconfig", drawtf.GenerateTfconfig)
 	r.POST("/start-gradio", drawtf.StartGradio)
+
+	// Start the Gin HTTP server on port 8082. This will listen for incoming HTTP requests.
 	r.Run(":8082")
 }
